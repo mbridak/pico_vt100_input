c72aef5b7f45ac5956beecda5eb589029b84bcae
mbridak <michael.bridak@gmail.com>
2022-11-21 07:57:36 -0800

    Documented code a bit more.
    Made a super cool DEC logo.
    I'm easily amused.

--- code.py
+++ code.py
@@ -52,13 +52,14 @@ def show():
 vt100.set80cols()
 vt100.cls()
 vt100.home()
+# vt100.autowrapoff()
 vt100.cursor_down()
-
 vt100.double_height_top()
-print("VT102 Test Screen")
+vt100.out("VT102 Test Screen")
+vt100.out("\r\n")
 vt100.double_height_bottom()
-print("VT102 Test Screen")
-
+vt100.out("VT102 Test Screen")
+# vt100.box(1,1,24,80)
 vt100.attr_off()
 vt100.addstr(10, 1, "Normal")
 vt100.attr_bold()
@@ -70,9 +71,36 @@ vt100.attr_off()
 vt100.attr_underline()
 vt100.addstr(13, 1, "Underline")
 vt100.attr_off()
+# DEC Logo
 vt100.attr_reverse()
-vt100.addstr(14, 1, "Reverse")
+vt100.addstr(14, 1, "d")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("i")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("g")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("i")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("t")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("a")
+vt100.sg0()
+vt100.out("x")
+vt100.ug0()
+vt100.out("l")
 vt100.attr_off()
+
+vt100.out(" Reverse")
 vt100.attr_blink()
 vt100.addstr(15, 1, "Blink")
 vt100.attr_off()
@@ -88,13 +116,20 @@ vt100.addstr(6, 11, "Radio Button [_]")
 radio_button = EditTextField(6, 25, 1)
 radio_button.set_bool(True)
 
+# input_fields holds a list of available input widgets that we can
+# TAB cycle through. When get_focus is called the current state of the
+# widget is displayed and awaits input. So we cycle through all the
+# widgets before calling the show function.
 input_fields = [input_alphanum, radio_button]
 for item in input_fields:
     item.get_focus()
 
-input_alphanum.get_focus()
+# out example function show will return True if the Enter key was
+# pressed to quit the input, or False if the Esc key was pressed.
+# You can then get the current text values or boolean states of the
+# widgets.
 result = show()
 
-print(f"The input dialog returned: {result}")
-print(f"The input field returned: {input_alphanum.text()}")
-print(f"The radio button was set to: {radio_button.get_state()}")
+vt100.out(f"The input dialog returned: {result}\r\n")
+vt100.out(f"The input field returned: {input_alphanum.text()}\r\n")
+vt100.out(f"The radio button was set to: {radio_button.get_state()}\r\n")
